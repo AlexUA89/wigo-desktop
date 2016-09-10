ea83362678da38d38d9c057eb3494bdc31948273
Pavel Marchuk <marchukpavelp@gmail.com>
2016-09-10 15:33:56 +0300

    Implement "get_events" function

--- facebook.py
+++ facebook.py
@@ -3,19 +3,29 @@ import requests
 
 
 TOKEN = 'EAACEdEose0cBAPUGwEAAgCKZAIC7WeVulsMLxcxK6byHLZARr8DC5v93tphF5PgleVHSEndXGZAgSS7rPHl656iF9XP0ZBlu0lKc5CVGChu0ykxGhDCItHP5H1txPKaOS2u8xpzSomXmk9bPPKShSGGB5S0TMaK91qZAUuRdqeQZDZD'
+
+URL = 'https://graph.facebook.com/search'
 LOCATIONS = ('kiev',)
 TYPE = 'event'
-URL = 'https://graph.facebook.com/search'
+ADDITIONAL = {'debug': 'all', 'format': 'json', 'limit': 1000}
 
-additional_params = {'debug': 'all', 'format': 'json', 'limit': 1000}
 
-for location in LOCATIONS:
-    params = {
-        'q': location,
-        'type': TYPE,
-        'access_token': TOKEN,
-    }
-    params.update(additional_params)
+def get_events(request_count=5):
+    events = []
+    for location in LOCATIONS:
+        params = {
+            'q': location,
+            'type': TYPE,
+            'access_token': TOKEN,
+        }
+        params.update(ADDITIONAL)
+        for _ in range(request_count):
+            response = requests.get(URL, params=params).json()
+            data = response['data']
+            if not data:
+                break
+            events += data
+            after = response['paging']['cursors']['after']
+            params[after] = after
 
-    response = requests.get(URL, params=params)
-    print response.json()
+    return events
